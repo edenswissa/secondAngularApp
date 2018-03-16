d956e2a39ef3ab1f7a82e6f1d87c020be302a064
Eden Swissa <edenswi67@gmail.com>
2018-03-16 19:28:45 +0200

    16.3.18

--- angularCourse/src/app/servers/servers.component.ts
+++ angularCourse/src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { setTimeout } from 'timers';
 
 @Component({
   selector: 'app-servers',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer:boolean = false;
+  serverCreationStatus: String = 'no server was created';
+  serverName : string = '';
+  serverCreated : boolean = false;
+  servers = [];
+
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit() {
   }
+
+  onAddServer()
+  {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
+
+  
 
 }
--- angularCourse/src/app/success-alert/success-alert.component.ts
+++ angularCourse/src/app/success-alert/success-alert.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessAlertComponent implements OnInit {
 
-  constructor() { }
+  allowButton : boolean = false;
+  buttonClick : string = "button not clicked yet";
+  oneWayForDataBinding : string='';
+  twoWayForDataBinding : string ="the other way to data binding";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowButton = true;
+    },2000);
+   }
 
   ngOnInit() {
+  }
+
+  onButtonClick()
+  {
+    this.buttonClick = "the button was cliked";
+  }
+
+  onInputEvent(event: Event)
+  {
+    this.oneWayForDataBinding = (<HTMLInputElement>event.target).value;
   }
 
 }
